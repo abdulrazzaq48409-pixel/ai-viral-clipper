279cf87c06aa1705a6cf96411f1dd02c0cdc7794
Abdul Razzaq <abdulrazzaq48409@gmail.com>
2026-05-08 11:50:34 +0500

    fix template path

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
