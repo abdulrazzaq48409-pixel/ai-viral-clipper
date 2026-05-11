9f57c0ddb7823697895dd0173aa0bffe3582d1a4
Abdul Razzaq <abdulrazzaq48409@gmail.com>
2026-05-11 14:17:46 +0500

    minimal railway fix

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template
-import os
 
-template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
-
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
